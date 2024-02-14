a865a6b90b93b8d992325e5408202b4be732f19e
Sebastian Jaramillo <hsjj2000@gmail.com>
2024-02-13 19:59:11 -0500

    Cross Origin

--- src/main/java/com/banquito/core/banking/seguridad/clientes/tarjetas/controller/TarjetaController.java
+++ src/main/java/com/banquito/core/banking/seguridad/clientes/tarjetas/controller/TarjetaController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banquito.core.banking.seguridad.clientes.tarjetas.dto.TarjetaDTO;
@@ -15,7 +14,9 @@ import com.banquito.core.banking.seguridad.clientes.tarjetas.services.TarjetaSer
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@CrossOrigin(origins = "", allowedHeaders = "", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin(origins = {"http://localhost:4200", "http://34.173.161.134:4201", "http://34.176.205.203:4202", 
+                        "http://34.176.102.118:4203", "http://34.176.137.180:4204"})
+//@CrossOrigin(origins = "", allowedHeaders = "", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 @RestController
 @RequestMapping("/api/v1/seguridad-tarjeta")
 public class TarjetaController {
